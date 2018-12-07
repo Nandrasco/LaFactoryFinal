cd3cd754986aad21ee91fd866e93ebe2e93a464f
Unknown <saidani.tanguy@gmail.com>
2018-12-07 10:00:36 +0100

    maj formateur

--- src/main/java/io/github/jhipster/application/domain/Formateur.java
+++ src/main/java/io/github/jhipster/application/domain/Formateur.java
@@ -19,6 +19,8 @@ import javax.persistence.Table;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 
 import java.io.Serializable;
